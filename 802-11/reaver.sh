d2365e27c17f22adcea89619b0d30481c56d2b16
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-03-23 22:03:18 +0700

    Create reaver.sh

---
+++ 802-11/reaver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#-------------------------------------------------------------
+# Tentang Script:
+#
+# * Nama file: reaver.sh
+# * Deskripsi: Script Bash untuk instal tool reaver secara otomatis
+# * Dibuat oleh: Rofi (Fixploit03)
+# * Github: https://github.com/fixploit03/installer_tools
+#--------------------------------------------------------------
+# Catatan:
+#
+# * Jalankan scriptnya sebagai root (sudo su)
+# * Pastikan Anda memiliki koneksi internet yang stabil
+#---------------------------------------------------------------
+# Sumber:
+#
+# * https://github.com/t6x/reaver-wps-fork-t6x
+#---------------------------------------------------------------
+
+apt-get update -y
+apt install build-essential libpcap-dev sqlite3 libsqlite3-dev git
+cd /opt
+git clone https://github.com/t6x/reaver-wps-fork-t6x.git
+cd reaver-wps-fork-t6x
+./configure
+make
+make install
