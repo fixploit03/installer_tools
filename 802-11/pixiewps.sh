c38cc631491723ef3941defff1920334ee979d85
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-03-23 22:12:55 +0700

    Create pixiewps.sh

---
+++ 802-11/pixiewps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#-------------------------------------------------------------
+# Tentang Script:
+#
+# * Nama file: pixiewps.sh
+# * Deskripsi: Script Bash untuk instal tool pixiewps secara otomatis
+# * Dibuat oleh: Rofi (Fixploit03)
+# * Github: https://github.com/fixploit03/installer_tools
+#--------------------------------------------------------------
+# Catatan:
+#
+# * Jalankan scriptnya sebagai root (sudo su)
+# * Pastikan Anda memiliki koneksi internet yang stabil
+#---------------------------------------------------------------
+# Sumber:
+#
+# * https://github.com/wiire-a/pixiewps
+#---------------------------------------------------------------
+
+apt-get update -y
+apt install build-essential libssl-dev git -y
+cd /opt
+git clone https://github.com/wiire-a/pixiewps
+cd pixiewps
+make
+make install
