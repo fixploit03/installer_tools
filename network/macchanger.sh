7c68258def1f1244466b8293b8fe86662ceda679
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-03-26 00:34:24 +0700

    Create macchanger.sh

---
+++ network/macchanger.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+#-------------------------------------------------------------
+# Tentang Script:
+#
+# * Nama file: macchanger.sh
+# * Deskripsi: Script Bash untuk instal tool macchanger secara otomatis
+# * Dibuat oleh: Rofi (Fixploit03)
+# * Github: https://github.com/fixploit03/installer_tools
+#--------------------------------------------------------------
+# Catatan:
+#
+# * Jalankan scriptnya sebagai root (sudo su)
+# * Pastikan Anda memiliki koneksi internet yang stabil
+#---------------------------------------------------------------
+# Sumber:
+#
+# * https://github.com/alobbs/macchanger
+#---------------------------------------------------------------
+
+apt-get update
+apt install build-essential libc6-dev autoconf automake libtool pkg-config git -y
+cd /opt
+git clone https://github.com/alobbs/macchanger
+cd macchanger
+./autogen.sh
+./configure
+make
+make install
+
+# - END -
