cf900dd598c8fd4e4732cccd14f7b9a4cf65cccf
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-03-25 19:27:55 +0700

    Create hcxdumptool.sh

---
+++ 802-11/hcxdumptool.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#-------------------------------------------------------------
+# Tentang Script:
+#
+# * Nama file: hcxdumptool.sh
+# * Deskripsi: Script Bash untuk instal tool hcxdumptool secara otomatis
+# * Dibuat oleh: Rofi (Fixploit03)
+# * Github: https://github.com/fixploit03/installer_tools
+#--------------------------------------------------------------
+# Catatan:
+#
+# * Jalankan scriptnya sebagai root (sudo su)
+# * Pastikan Anda memiliki koneksi internet yang stabil
+#---------------------------------------------------------------
+# Sumber:
+#
+# * https://github.com/ZerBea/hcxdumptool
+#---------------------------------------------------------------
+
+apt-get update -y
+apt-get install gcc make libpcap libpcap-dev -y
+cd /opt
+git clone https://github.com/ZerBea/hcxdumptool
+cd hcxdumptool
+make -j $(nproc)
+make install
